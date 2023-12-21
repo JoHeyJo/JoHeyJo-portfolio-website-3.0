dd7d99b231dda80e2e97769359221a77cbff0df2
jf <jpf0628@gmail.com>
2023-12-21 11:34:24 -0800

    adding Banner, styles, compoents

---
+++ src/utils/view.tsx
@@ -0,0 +1,26 @@
+import '../styles/Banner.css'
+
+function renderTypeScript(windowWidth: number, isLandscape: boolean) {
+  return windowWidth > 915
+    ? <span><span className="tech">TypeScript< span className="pipes" >|</span> </span></span >
+    : <span><i className="devicon-typescript-plain" > </i><span className="pipes">|</span > </span>
+}
+function renderReact(windowWidth: number, isLandscape: boolean) {
+  return windowWidth > 935
+    ? <span><span className="tech">React< span className="pipes" >| </span></span></span >
+    : <span><i className="devicon-react-original" > </i><span className="pipes">|</span > </span>
+}
+
+function renderPython(windowWidth: number, isLandscape: boolean) {
+  return windowWidth > 955
+    ? <span><span className="tech">Python < span className="pipes" >| </span></span></span >
+    : <span><i className="devicon-python-plain" > </i><span className="pipes">|</span > </span>
+}
+
+function renderFlask(windowWidth: number, isLandscape: boolean) {
+  return windowWidth > 995
+    ? <span className="tech"> Flask </span>
+    : <i className="devicon-flask-original" > </i>
+}
+
+export { renderTypeScript, renderReact, renderPython, renderFlask }
