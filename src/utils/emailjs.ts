04e2f5ba931e267543152f276e145b33a005ca21
jf <jpf0628@gmail.com>
2024-02-03 16:29:34 -0800

    wip: application is not receiving env variable

--- src/utils/emailjs.ts
+++ src/utils/emailjs.ts
@@ -1,8 +1,10 @@
+"use server"
 import emailjs from '@emailjs/browser';
 
-const SERVICE_ID = process.env.REACT_APP_SERVICE_KEY ?? "";
-const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID ?? "";
-const PUBLIC_KEY = process.env.REACT_APP_PUBLIC_KEY ?? "";
+const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_KEY ?? "";
+const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID ?? "";
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY ?? "";
+console.log(SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY)
 
 function Emailjs(e:any) {
 
