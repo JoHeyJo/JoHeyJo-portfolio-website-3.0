7a144d267dc3c2dc0e56c3254f8792bff6209f3b
jf <jpf0628@gmail.com>
2024-02-10 16:04:22 -0800

    migrated all working components from website 3.0 to next js app

--- src/styles/ContainerStyles.ts
+++ src/styles/ContainerStyles.ts
@@ -1,28 +1,28 @@
-// import { CSSProperties } from 'react';
+import { CSSProperties } from 'react';
 
-// export const containerStyle: CSSProperties = {
-//   border: '1px solid #000',
-//   padding: '10px',
-// };
+export const containerStyle: CSSProperties = {
+  // border: '1px solid #000',
+  // padding: '10px',
+};
 
-// export const rowStyle: CSSProperties = {
-//   border: '1px solid #000',
-//   // display: 'flex',
-// };
+export const rowStyle: CSSProperties = {
+  // border: '1px solid #000',
+  // display: 'flex',
+};
 
-// export const navColStyle: CSSProperties = {
-//   border: '1px solid #000',
-//   padding: '10px',
-//   // width: '200px',  
-// };
+export const navColStyle: CSSProperties = {
+  // border: '1px solid #000',
+  // padding: '10px',
+  // width: '200px',  
+};
 
-// export const mainColStyle: CSSProperties = {
-//   border: '1px solid #000',
-//   padding: '10px',
-//   // width: '100%',
-// };
+export const mainColStyle: CSSProperties = {
+  // border: '1px solid #000',
+  // padding: '10px',
+  // width: '100%',
+};
 
-// export const sectionStyle: CSSProperties = {
-//   border: '1px solid #000',
-//   padding: '10px',
-// };
+export const sectionStyle: CSSProperties = {
+  // border: '1px solid #000',
+  // padding: '10px',
+};
