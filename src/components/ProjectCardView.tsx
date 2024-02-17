730e343eba07ff98e879f9bf95ad12f184a83317
jf <jpf0628@gmail.com>
2024-02-16 16:05:41 -0800

    ts

---
+++ src/components/ProjectCardView.tsx
@@ -0,0 +1,25 @@
+import '../styles/ProjectCardView.css';
+import OverlayCard from './OverlayCard';
+import { Project } from '../utils/types';
+import Image from 'next/image';
+
+type ProjectCardProp = Project & {
+  index: number;
+}
+
+/** Project Image displayed in thumbnail view
+ * 
+ * Project -> ProjectCardView -> OverlayCard
+ */
+function ProjectCardView({ index, name, image, links, cardHeader, cardSummary, tech }: ProjectCardProp) {
+  return (
+    <div className="ProjectCardView-container">
+      <Image className="ProjectCardView-image" src={image} alt={`${name} image`} />
+      <div className="ProjectCardView-overlay">
+        <OverlayCard index={index} link={links.demo} header={cardHeader} summary={cardSummary} tech={tech} />
+      </div>
+    </div>
+  )
+}
+
+export default ProjectCardView;
