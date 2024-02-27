32344baf7edf3f468ebc5c48ca63bd40f4e79550
jf <jpf0628@gmail.com>
2024-02-27 10:26:25 -0800

    fixed bugly overlay

--- src/utils/projectData.ts
+++ src/utils/projectData.ts
@@ -8,7 +8,7 @@ export const bugly: Project = {
   name: "Bugly",
   image: buglyImg,
   cardHeader: "Blog Application",
-  cardSummary: "React application that catalogs projects and corresponding bugs. Retro minimalist design aesthetic with modern dynamic functionality. Currently active and tracking progress on live projects",
+  cardSummary: "React application, catalogs projects and corresponding bugs. Retro minimalist design aesthetic with modern dynamic functionality. Currently active and tracking progress on live projects",
   tech: ["React", "TypeScript", "Python", "Flask", "SQLAlchemy", "Bootstrap"],
   allTech: "React | TS | Python | Flask | SQLAlchemy | Bootstrap",
   description: "Browse projects and their corresponding bugs.",
